8a85b3ba767e0a95ea16eff9ec481934f51eb043
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-08-29 07:14:46 +0400

    feat: add logic to monitor the images progress

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ import {ieFix} from './vendor/ie-fix';
 import {vhFix} from './vendor/vh-fix';
 import {actualYear} from './modules/actualYear';
 import header from './components/header';
+import home from './pages/home';
 import lazyLoading from './modules/lazyLoading';
 import scrollToAnchor from './modules/scrollToAnchor';
 
@@ -14,4 +15,6 @@ actualYear();
 scrollToAnchor.init();
 
 header.init();
+home.init();
+
 lazyLoading.init();
---
+++ src/js/pages/home.js
@@ -0,0 +1,71 @@
+import {getBestSource} from '../modules/srcSet';
+
+let allImagesTotalBytes;
+let allImagesLoadedBytes;
+
+/**
+ * Обработать прогресс загрузки изображения
+ * @public
+ * @param {ProgressEvent} event событие прогресса
+ */
+function handleImageProgress(event) {
+	let isNotTotalSent = true;
+
+	if (event.lengthComputable) {
+		if (isNotTotalSent) {
+			allImagesTotalBytes = event.total;
+			isNotTotalSent = false;
+		}
+
+		allImagesLoadedBytes += event.loaded;
+	}
+}
+
+/**
+ * Инициализировать страницу
+ * @public
+ */
+function init() {
+	document.addEventListener('DOMContentLoaded', async () => {
+		const images = document.querySelectorAll('img[data-src]');
+		const dpr = window.devicePixelRatio;
+
+		for (let i = 0; i < images.length; i++) {
+			let image = images[i];
+
+			/** Ссылка на изображение */
+			const url = getBestSource(image, dpr);
+
+			/** Инициализация запроса */
+			const xhr = new XMLHttpRequest();
+
+			/** Выполнить запрос */
+			xhr.open('GET', url, true);
+
+			/** Перевести изображение в формат BLOB */
+			xhr.responseType = 'blob';
+
+			/** Выполнить код во время загрузки изображения */
+			xhr.addEventListener('progress', handleImageProgress);
+
+			const handleImageUpload = () => {
+				if (xhr.status === 200) {
+					const blob = xhr.response;
+					const imgObjectURL = URL.createObjectURL(blob);
+					image.src = imgObjectURL;
+				} else {
+					throw new Error(`Ошибка загрузки изображения ${url}. Статус: ${xhr.status}`);
+				}
+			};
+
+			/** Выполнить код после загрузки изображения */
+			xhr.addEventListener('load', handleImageUpload);
+
+			xhr.send();
+		}
+	});
+}
+
+export default {
+	init,
+};
